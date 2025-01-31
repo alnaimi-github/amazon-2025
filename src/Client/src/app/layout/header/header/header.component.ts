670bfe973039eed9a8513ad47c8b5798dab0da93
alnaimi-github <alnymybdalwas1@gmail.com>
2025-01-31 21:28:36 +0300

    Add loading interceptor and busy service; update header to show progress bar

--- src/Client/src/app/layout/header/header/header.component.ts
+++ src/Client/src/app/layout/header/header/header.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { BusyService } from './../../../core/services/busy.service';
+import { Component, inject } from '@angular/core';
 import {  MatBadge } from '@angular/material/badge';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { MatProgressBar } from '@angular/material/progress-bar';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 
@@ -12,12 +14,14 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
     MatButton,
     MatBadge,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    MatProgressBar
 
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  busyService = inject(BusyService);
 
 }
