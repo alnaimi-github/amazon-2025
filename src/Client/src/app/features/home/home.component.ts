0401b78b2b951fbef929adcb46fda7dda0cc80a5
alnaimi-github <alnymybdalwas1@gmail.com>
2025-02-08 00:04:05 +0300

    Enhance home component UI; replace placeholder with a styled card layout, add welcome message, and implement navigation button

--- src/Client/src/app/features/home/home.component.ts
+++ src/Client/src/app/features/home/home.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    RouterModule
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
 
