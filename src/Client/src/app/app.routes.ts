bea7e29c0ece6ccfda24ae2e918ab47a6fb5801c
alnaimi-github <alnymybdalwas1@gmail.com>
2025-02-07 23:15:46 +0300

    Add empty cart guard to checkout route; prevent navigation if cart is empty and display error message

--- src/Client/src/app/app.routes.ts
+++ src/Client/src/app/app.routes.ts
@@ -10,13 +10,14 @@ import { CheckoutComponent } from './featuress/checkout/checkout.component';
 import { LoginComponent } from './featuress/account/login/login.component';
 import { RegisterComponent } from './featuress/account/register/register.component';
 import { authGuard } from './core/guards/auth.guard';
+import { emptyCartGuard } from './core/guards/empty-cart.guard';
 
 export const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'shop', component: ShopComponent},
   {path:'shop/:id', component: ProductDetailsComponent},
   {path:'cart', component: CartComponent},
-  {path:'checkout', component: CheckoutComponent, canActivate: [authGuard]},
+  {path:'checkout', component: CheckoutComponent, canActivate: [authGuard, emptyCartGuard]},
   {path:'account/login', component: LoginComponent},
   {path:'account/register', component: RegisterComponent},
   {path:'test-error', component: TestErrorComponent},
