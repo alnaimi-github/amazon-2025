387b892f4c23fc487c099e264ada699a7cf8ddd8
alnaimi-github <alnymybdalwas1@gmail.com>
2025-02-24 22:07:22 +0300

    Add delivery method selection to checkout; update cart totals and integrate delivery method handling

--- src/Client/src/app/featuress/checkout/checkout-delivery/checkout-delivery.component.ts
+++ src/Client/src/app/featuress/checkout/checkout-delivery/checkout-delivery.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MatRadioModule } from '@angular/material/radio';
 import { CheckoutService } from '../../../core/services/checkout.service';
 import { CurrencyPipe } from '@angular/common';
+import { CartService } from '../../../core/services/cart.service';
+import { DeliveryMethod } from '../../../shared/models/deliveryMethod';
 
 @Component({
   selector: 'app-checkout-delivery',
@@ -15,9 +17,28 @@ import { CurrencyPipe } from '@angular/common';
 export class CheckoutDeliveryComponent implements OnInit {
 
   checkoutService = inject(CheckoutService);
-
+  cartService = inject(CartService);
+  
   ngOnInit(): void {
-   this.checkoutService.getDeliveryMethods().subscribe();
+   this.checkoutService.getDeliveryMethods().subscribe({
+      next: methods => {
+         if (this.cartService.cart()?.deliveryMethodId) {
+          const method = methods.find(x => x.id === this.cartService.cart()?.deliveryMethodId);
+          if (method) {
+            this.cartService.selectedDelivery.set(method);
+          }
+         }
+        }
+   });
+  }
+
+  updateDeliveryMethod(method: DeliveryMethod) {
+    this.cartService.selectedDelivery.set(method);
+    const cart = this.cartService.cart();
+    if (cart) {
+      cart.deliveryMethodId = method.id;
+      this.cartService.SetCart(cart);
+    }
   }
 }
 
